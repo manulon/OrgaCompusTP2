ff2519d28988e959f80f7b7155e133ec6afee8d6
federicoburman <fburman@fi.uba.ar>
2020-11-28 21:18:02 +0000

    matrx .h

---
+++ matrix.h
@@ -0,0 +1,22 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef struct matrix {
+		size_t rows;
+			size_t cols;
+				double* array;
+} matrix_t;
+
+/* prototypes */
+char* read_line(FILE*);
+
+matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2, int bs);
+
+matrix_t* create_matrix(size_t rows, size_t cols);
+
+void destroy_matrix(matrix_t* m);
+
+int print_matrix(FILE* fp, matrix_t* m);
+
+#endif
+
